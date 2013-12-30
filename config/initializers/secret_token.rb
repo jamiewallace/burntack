09fa587668fafc64bdb3e5fb1841be138bd31507
jamiewallace <wallacejamiec@gmail.com>
2013-12-30 15:11:39 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Burntack::Application.config.secret_token = 'b8fb056139f45084cc3604af6b619726809aa1df21360e570bb58f5a7b45c1597948a29b62ea713facfb7d4c290f1e75caf39b8e5a741b5b52f52fa446aeba69'
